14397cae68192db83d340fa85eefe1c548392921
ricalungu <rica.lungu@gmail.com>
2026-01-16 18:11:43 +0100

    adjusted title, authors and description modified column Pclass

--- R/01_data_preparation.R
+++ R/01_data_preparation.R
@@ -1,9 +1,27 @@
-# Titel: Titanic Preprocessing Part 1
-# Autorin: Meriam
-# Beschreibung:
-#  Einlesen des Titanic-Datensatzes
-#  Vorbereitung für die Aufbereitung (Titel/Anrede, Codierung)
-# Datum: 13.01.2026
+# Title: Titanic Preprocessing
+# Authors: Meriam & Rica
+# Description: 
+#  - read dataset titanic.csv
+#  - create new columns title and modify existing columns
+#  - remove columns which are not needed for further analysis
+#  - save adjusted dataset as titanic_modified.csv
+
+# Date: 13.01.2026
+
 df <- read.csv("data/raw/titanic.csv", stringsAsFactors = FALSE)
 str(df)
 summary(df)
+
+titanic_modified <- df
+
+
+############## recode Pclass as ordered factor ##############
+
+# check if column Pclass only contains numbers
+check_Pclass <- unique(titanic_modified$Pclass)
+
+# recode
+titanic_modified$Pclass <- ordered(titanic_modified$Pclass)
+
+#############################################################
+
